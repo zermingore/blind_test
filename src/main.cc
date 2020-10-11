5483dcd10d9054785b23447f69184d58cf6282b6
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-10-11 14:50:13 +0200

    [Feature] Added a colored point moving accross the screen

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
+#include <iostream>
+
 #include "imgui.h"
 #include "imgui-SFML.h"
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
@@ -14,10 +17,17 @@ int main()
   window.setVerticalSyncEnabled(true);
   ImGui::SFML::Init(window);
 
+  sf::CircleShape focusPoint;
+  focusPoint.setRadius(30.f);
+  focusPoint.setFillColor({255, 0, 0});
+  focusPoint.setPosition(30.f, 30.f);
 
-  sf::Color bgColor;
 
-  float color[3] = { 0.f, 0.f, 0.f };
+  sf::Color bgColor = { 0, 255, 0 };
+  sf::Color focusPointColor = { 255, 0, 0 };
+
+  float color[3] = { 0.f, 1.f, 0.f };
+  float colorPoint[3] = { 1.f, 0.f, 0.f };
 
   sf::Clock deltaClock;
   while (window.isOpen())
@@ -33,11 +43,15 @@ int main()
       }
     }
 
+    focusPoint.setFillColor(focusPointColor);
+    focusPoint.setPosition((static_cast<int> (focusPoint.getPosition().y) + 1) % window.getSize().x,
+                           (static_cast<int> (focusPoint.getPosition().y) + 1) % window.getSize().y);
+
     ImGui::SFML::Update(window, deltaClock.restart());
 
 
     // Dear ImGUi
-    ImGui::Begin("Sample window");
+    ImGui::Begin("Settings");
 
     // Background color edit
     if (ImGui::ColorEdit3("Background color", color))
@@ -47,9 +61,18 @@ int main()
       bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
     }
 
+    if (ImGui::ColorEdit3("Point color", colorPoint))
+    {
+      focusPointColor.r = static_cast<sf::Uint8>(colorPoint[0] * 255.f);
+      focusPointColor.g = static_cast<sf::Uint8>(colorPoint[1] * 255.f);
+      focusPointColor.b = static_cast<sf::Uint8>(colorPoint[2] * 255.f);
+    }
+
     ImGui::End();
 
     window.clear(bgColor); // fill background with color
+
+    window.draw(focusPoint);
     ImGui::SFML::Render(window);
     window.display();
   }
