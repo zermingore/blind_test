cc92f310c91981fc953a30c69a82b5f490de9a8b
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-10-11 14:26:28 +0200

    [Clean] 60 FPS limit; minor clean

--- src/main.cc
+++ src/main.cc
@@ -9,45 +9,45 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "");
+  sf::RenderWindow window(sf::VideoMode(640, 480), "BlindTest");
+  window.setFramerateLimit(60);
   window.setVerticalSyncEnabled(true);
   ImGui::SFML::Init(window);
+
 
   sf::Color bgColor;
 
   float color[3] = { 0.f, 0.f, 0.f };
 
-  // let's use char array as buffer, see next part
-  // for instructions on using std::string with ImGui
-  const char *windowTitle = "BlindTest";
-
-  window.setTitle(windowTitle);
-  window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
   sf::Clock deltaClock;
-  while (window.isOpen()) {
+  while (window.isOpen())
+  {
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (window.pollEvent(event))
+    {
       ImGui::SFML::ProcessEvent(event);
 
-      if (event.type == sf::Event::Closed) {
+      if (event.type == sf::Event::Closed)
+      {
         window.close();
       }
     }
 
     ImGui::SFML::Update(window, deltaClock.restart());
 
-    ImGui::Begin("Sample window"); // begin window
+
+    // Dear ImGUi
+    ImGui::Begin("Sample window");
 
     // Background color edit
-    if (ImGui::ColorEdit3("Background color", color)) {
-      // this code gets called if color value changes, so
-      // the background color is upgraded automatically!
+    if (ImGui::ColorEdit3("Background color", color))
+    {
       bgColor.r = static_cast<sf::Uint8>(color[0] * 255.f);
       bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
       bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
     }
 
-    ImGui::End(); // end window
+    ImGui::End();
 
     window.clear(bgColor); // fill background with color
     ImGui::SFML::Render(window);
