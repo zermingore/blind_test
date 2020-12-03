89da4df5977eaa5b8d194553c3fa2e7202705621
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-12-03 18:50:41 +0100

    [Tools] Initializing the logger

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,14 @@
 #include <future>
 
+#include <tools/Debug.hh>
 #include "InputsManager.hh"
 #include "GraphicsEngine.hh"
 
 
-
 int main()
 {
+  Debug::init();
+
   std::atomic_bool stop_events_listener{false};
   auto inputs = std::async(
     std::launch::async,
